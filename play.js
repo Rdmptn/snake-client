95a83400831443032a58787a4cc2187a0251abee
Cameron <cameron_uniac@hotmail.com>
2021-06-30 12:56:27 -0400

    Moved connect function to client.js, changed syntax to shorthand notation

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", (data) => {
-  // code that does something when the connection is first established
-  console.log(data);
-});
-
-  return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
